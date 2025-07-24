63dfcef5948a79243180b094a6e5f3ecf100b86c
GitHub Action <action@github.com>
2025-07-24 15:49:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/svk014/autodbgjetbrains/MyBundle.kt
+++ src/main/kotlin/com/github/svk014/autodbgjetbrains/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.svk014.autodbgjetbrains
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/svk014/autodbgjetbrains/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/svk014/autodbgjetbrains/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.svk014.autodbgjetbrains.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.svk014.autodbgjetbrains.MyBundle
+import com.github.svk014.autodbgjetbrains.services.MyProjectService
 import javax.swing.JButton
 
 
